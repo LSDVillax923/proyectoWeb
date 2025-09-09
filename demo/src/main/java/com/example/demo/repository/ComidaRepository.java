433362efd35482991689cdc5822da0a309778687
LSD_Villax <andresvillabon@gmail.com>
2025-09-09 08:31:25 -0500

    base de datos

--- demo/src/main/java/com/example/demo/repository/ComidaRepository.java
+++ demo/src/main/java/com/example/demo/repository/ComidaRepository.java
@@ -13,11 +13,20 @@ public class ComidaRepository {
     private Map<Integer, Comida> comidas = new HashMap<>();
 
     public ComidaRepository() {
-        comidas.put(1, new Comida(1, "Ejemplo", "Descripción de ejemplo", 9.99, "http://example.com/imagen.jpg"));
-        comidas.put(2, new Comida(2, "Ejemplo 2", "Descripción de ejemplo 2", 19.99, "http://example.com/imagen2.jpg"));
-        comidas.put(3, new Comida(3, "Ejemplo 3", "Descripción de ejemplo 3", 29.99, "http://example.com/imagen3.jpg"));
-        comidas.put(4, new Comida(4, "Ejemplo 4", "Descripción de ejemplo 4", 39.99, "http://example.com/imagen4.jpg"));
-
+       comidas.put(1, new Comida(1, "Hamburguesa Gourmet", "Jugosa carne de res, queso maduro, cebolla caramelizada y pan artesanal.", 38000.0,
+                "../public/images/platos/hamburguesa.png"));
+        comidas.put(2, new Comida(2, "Salchipapa Especial", "Papas fritas artesanales con salchicha  y salsas de la casa.", 28000.0,
+                "../public/images/platos/salchipapa.png"));
+        comidas.put(3, new Comida(3, "Carne Asada", "Corte de res a la parrilla acompañado de vegetales salteados.", 45000.0,
+                "../public/images/platos/carneAsada.png"));
+        comidas.put(4, new Comida(4, "Pollo Asado", "Pechuga de pollo marinada y asada lentamente con hierbas frescas.", 32000.0,
+                "../public/images/platos/pechugaAsada.png"));
+        comidas.put(5, new Comida(5, "Ceviche", "Pescado fresco marinado en jugo de limón con cebolla y cilantro", 45000.0,
+                "../public/images/platos/ceviche.png"));
+        comidas.put(6, new Comida(6, "Pasta Alfredo", "Pasta fresca con salsa Alfredo cremosa y queso parmesano.", 38000.0,
+                "../public/images/platos/pasta.png"));
+        comidas.put(7, new Comida(7, "Pizza Gourmet", "Masa artesanal, salsa de tomate fresco y ingredientes seleccionados.", 40000.0,
+                "../public/images/platos/pizza.png"));
     }
     
     public Comida findById(Integer id) {
