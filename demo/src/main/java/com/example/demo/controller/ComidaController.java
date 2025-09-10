5b66205c5b8d8d1d1bee0aef581af945ad9ac690
LSD_Villax <andresvillabon@gmail.com>
2025-09-10 14:41:37 -0500

    restauracion

--- demo/src/main/java/com/example/demo/controller/ComidaController.java
+++ demo/src/main/java/com/example/demo/controller/ComidaController.java
@@ -1,26 +1,31 @@
 package com.example.demo.controller;
 
 import com.example.demo.entities.Comida;
+import com.example.demo.service.ComidaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-import com.example.demo.entities.Comida;
 
-@RequestMapping("/comida")
 @Controller
+@RequestMapping("/comida")
 public class ComidaController {
+
+    @Autowired
+    private ComidaService comidaService;
+
+    @GetMapping("/lista")
+    public String listaComidas(Model model) {
+        model.addAttribute("comidas", comidaService.searchAll());
+        return "listaComidas";
+    }
 
     @GetMapping("/infoComida")
     public String infoComidaPage(Model model) {
-
         Comida comida = new Comida(1, "Ejemplo", "Descripción de ejemplo", 9.99, "http://example.com/imagen.jpg");
-
         model.addAttribute("comida", comida);
-
         return "infoComida";
     }
-
 
 }
