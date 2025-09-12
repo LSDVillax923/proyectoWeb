daf810a93a00c402ee5ba3400ead7bb3c84d9252
LSD_Villax <andresvillabon@gmail.com>
2025-09-11 19:50:18 -0500

    ???

--- demo/src/main/java/com/example/demo/controller/NavegacionController.java
+++ demo/src/main/java/com/example/demo/controller/NavegacionController.java
@@ -9,37 +9,33 @@ import com.example.demo.service.ComidaService;
 @Controller
 public class NavegacionController {
 
-@Autowired
-private ComidaService comidaService;
+    @Autowired
+    private ComidaService comidaService;
 
-@GetMapping("/inicio")
-public String inicioPage() {
-return "inicio";
+    @GetMapping("/inicio")
+    public String inicioPage() {
+        return "inicio";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/menu")
+    public String menuPage(Model model) {
+        model.addAttribute("comidas", comidaService.searchAll());
+        return "menu";
+    }
+
+    @GetMapping("/sign_in")
+
+    public String signInPage() {
+        return "sign_in";
+    }
+
+    @GetMapping("/admin/login")
+    public String adminLoginPage() {
+        return "admin";
+    }
 }
-
-
-@GetMapping("/login")
-public String loginPage() {
-return "login";
-}
-
-
-@GetMapping("/menu")
-public String menuPage(Model model) {
-model.addAttribute("comidas", comidaService.searchAll());
-return "menu";
-}
-
-@GetMapping("/sign_in")
-
-public String signInPage() {
-return "sign_in";
-}
-
-@GetMapping("/admin/login")
-public String adminLoginPage(){
-    return "admin";
-}
-}
-
-
