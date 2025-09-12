fe6228c50dc411dfc338a84eda724dd15b022390
LSD_Villax <andresvillabon@gmail.com>
2025-09-11 21:34:48 -0500

    cardinalidades 4

--- demo/src/main/java/com/example/demo/entities/Pedido.java
+++ demo/src/main/java/com/example/demo/entities/Pedido.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 
 @Entity
@@ -17,7 +18,7 @@ public class Pedido {
     private Long id;
     private String estado;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
